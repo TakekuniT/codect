f81666093bc34b10f517a86e78b9ecceaf43785c
amissimer <alivia.missimer04@gmail.com>
2024-09-01 16:07:37 -0400

    update database + auth

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,6 +1,13 @@
+"use client";
 import Sidenavbar from "@/components/sidenavbar"
+import { useAuthState, useSignOut } from "react-firebase-hooks/auth"
+import { auth } from "@/lib/firebase";
 
 export default function Home () {
+
+    const [user, loading] = useAuthState(auth);
+    const [signOut] = useSignOut(auth);
+
     return (
         <div className="bg-[#f6f6f6] h-screen">
             
