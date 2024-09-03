cbc2e08ab06127fb8c7cfde7ef21f99dc59c074d
amissimer <alivia.missimer04@gmail.com>
2024-09-03 19:57:12 -0400

    go to profile

--- components/header.tsx
+++ components/header.tsx
@@ -1,12 +1,17 @@
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "@/lib/firebase";
+import { useRouter } from "next/navigation";
 
 export default function Header(){
-  const [user, loading] = useAuthState(auth);
+  const [user] = useAuthState(auth);
+  const router = useRouter(); 
+  const profile = () => {
+    router.push('/profile')
+  }
   return (
     <div className="h-[8%] w-screen bg-white flex flex-row items-center justify-between">
       <p className="ml-9 font-body text-black font-medium text-xl">Hi, { user?.displayName || user?.email} 👋</p>
-      <img className="h-10 w-10 bg-gray-500 mr-4 rounded-3xl"/>
+      <img onClick={profile} className="h-10 w-10 bg-gray-500 mr-4 rounded-3xl"/>
     </div>
   )
 }
