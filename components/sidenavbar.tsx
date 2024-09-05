0a251f051532fea40bdb97e9ec3e29270738bb42
samantha <samanthap8675@gmail.com>
2024-09-05 15:59:37 -0400

    removed location

--- components/sidenavbar.tsx
+++ components/sidenavbar.tsx
@@ -3,11 +3,14 @@ import { House,UserPen, PanelsTopLeft  } from 'lucide-react';
 export default function Sidenavbar () {
     return (
         <div className="w-[5%] bg-white h-[100%] flex flex-col gap-4 items-center pt-8">
-            <House />
+            <a href="/home">
+                <House />
+
+            </a>
+            
             <UserPen/>
             <a href="/projects">
                 <PanelsTopLeft/>
-
             </a>
             
         </div>
