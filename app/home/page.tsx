bdcafa8836550e9ee4ea1e0a1865a5a9636eed26
amissimer <alivia.missimer04@gmail.com>
2024-09-05 18:27:59 -0400

    home page loading, signin, and signedout states

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -3,16 +3,37 @@ import Sidenavbar from "@/components/sidenavbar"
 import Header from "@/components/header";
 import { useAuthState, useSignOut } from "react-firebase-hooks/auth"
 import { auth } from "@/lib/firebase";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { Loader } from "lucide-react";
+import { SignedIn } from "@/components/signed-in";
 
 export default function Home () {
-
+    const router = useRouter(); 
     const [user, loading] = useAuthState(auth);
     const [signOut] = useSignOut(auth);
-
+    useEffect(() => {
+        if (!loading) {
+            if (!user) {
+                router.push('/login');
+            }
+        }
+    }, [loading, user, router]);
     return (
-        <div className="bg-[#f6f6f6] h-screen">
-            <Header/>
-            <Sidenavbar/>
-        </div>
+        <>
+            {loading ? 
+            <div className="flex justify-center items-center w-screen h-screen">
+                <Loader className="animate-spin w-10 h-10"></Loader> 
+            </div>
+            
+            : <></>}
+            <SignedIn>
+                <div className="bg-[#f6f6f6] h-screen">
+                <Header/>
+                <Sidenavbar/>
+                </div>
+            </SignedIn>
+        </>
+
     )
 }
