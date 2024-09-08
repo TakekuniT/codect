e57a7c22d01dc3b603a91e48b6ceeba393b7520c
amissimer <alivia.missimer04@gmail.com>
2024-09-08 19:12:17 -0400

    update profile

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,22 +1,43 @@
 "use client";
+import Header from "@/components/header";
 import Sidenavbar from "@/components/sidenavbar";
 import { SignedIn } from "@/components/signed-in";
 import { auth } from "@/lib/firebase";
+import { getCurrentUser, getProfile } from "@/services/profile";
 import { Loader } from "lucide-react";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
+import type { Profile } from "@/types/profile";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+import { updateProfile as updateUser } from "firebase/auth";
+import { updateProfile } from "@/services/profile";
 
 export default function Profile () {
   const router = useRouter(); 
   const [user, loading] = useAuthState(auth); 
+  const [currProfile, setCurrProfile] =  useState<Profile | null>(null); 
+  const [addName, setName] = useState("");
   useEffect(() => {
       if (!loading) {
           if (!user) {
               router.push('/login');
+          }else{
+            getCurrentUser(user).then(profile => {
+              setCurrProfile(profile);
+            }).catch(error =>{
+              console.error(error);
+            })
           }
       }
   }, [loading, user, router]);
+  const updateName = () => {
+    updateProfile(, {
+      name: addName
+    })
+  }
 
     return (
     <>
@@ -27,6 +48,7 @@ export default function Profile () {
             
             : <></>}
         <SignedIn>
+          <Header/>
        <div className="bg-[#f6f6f6] h-[200vh] flex">
        <Sidenavbar/>
         <div className="flex flex-col relative">
@@ -35,10 +57,29 @@ export default function Profile () {
           <div className="bg-white w-[55rem] h-[14rem] ml-12">
             <div className="flex flex-col w-[14rem] h-[12rem] items-start ml-4 font-body">
               <div className="mt-[6rem] font-semibold text-lg">
-                Name Goes Here
+                {currProfile?.name !== "" ? currProfile?.name : 
+                <Card className="w-[350px]">
+                <CardHeader>
+                  <CardTitle>Add Name</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <form>
+                    <div className="grid w-full items-center gap-4">
+                      <div className="flex flex-col space-y-1.5">
+                        <Label htmlFor="name">Name</Label>
+                        <input id="name" value={addName} onChange={(e) => setName(e.target.value)} placeholder="First and Last Name"/>
+                      </div>
+                    </div>
+                  </form>
+                </CardContent>
+                <CardFooter className="flex justify-between">
+                  <Button variant="outline" onClick={updateName}>Submit</Button>
+                </CardFooter>
+              </Card>
+                }
               </div>
               <div className="text-sm font-light mb-2">
-                @username
+                {currProfile?.userName !== "" ? "@"+currProfile?.userName : null}
               </div>
               <button className="rounded bg-white border-black border-[0.75px] font-light py-1 px-2 text-xs">
                 + Connect
